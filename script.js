9dcf07f958d1ae93b62680fb16111f0b03e5fc61
Corey96 <0coreyy@gmail.com>
2022-04-25 13:25:17 +0100

    removed duplicated code

--- script.js
+++ script.js
@@ -1,68 +1,80 @@
 // This shows a live date and time counter using jQuery and moment library
-var todayDate = moment().format('dddd, MMM Do YYYY, h:mm:ss a');
+var todayDate = moment().format("dddd, MMM Do YYYY, h:mm:ss a");
 $("#currentDay").html(todayDate);
 
-
 function timeTracker() {
-    //get current number of hours.
-    var timeNow = moment().hour();
+  //get current number of hours.
+  var timeNow = moment().hour();
 
-    //loops through time blocks
-    $(".time-block").each(function() {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+  //loops through time blocks
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+    if (blockTime < timeNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (blockTime === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
 
-        if (blockTime < timeNow) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
-        else if (blockTime === timeNow) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("past");
-        }
-        else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
+//Change time block colors to represent past/present/future
+let currentHour = moment().format("H");
+function pastPresentFuture() {
+  currentHour = moment().format("H");
+  for (i = 0; i < hoursArray.length; i++) {
+    if (currentHour == i + 8) {
+      hoursArray[i].addClass("present");
+    } else if (currentHour < i + 8) {
+      hoursArray[i].addClass("future");
+    } else {
+      hoursArray[i].addClass("past");
+    }
+  }
+}
 
-        }
-     })}
+// initial render of past/present/future colours
+timeTracker();
 
-    // initial render of past/present/future colours
-    timeTracker();
-
-    // rerun every minute so it updates throughout the day
-    setInterval(timeTracker, 60000);  
+// rerun every minute so it updates throughout the day
+setInterval(timeTracker, 60000);
 
 // Saving user inout to local storage
-let inputArray = $('input');
+let inputArray = $("input");
 let inputValues = [];
 
 function saveInput(e) {
-    // save button goes on save
-    $(e.target).css('display', 'none');
-    inputValues = [];
-    for (let i = 0; i < inputArray.length; i++) {
-        inputValues.push(inputArray[i].value);
-        localStorage.setItem("timeInputs", JSON.stringify(inputValues));
-    }}
+  // save button goes on save
+  $(e.target).css("display", "none");
+  inputValues = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    inputValues.push(inputArray[i].value);
+    localStorage.setItem("timeInputs", JSON.stringify(inputValues));
+  }
+}
 
 // Local storage
 if (localStorage.getItem("timeInputs") == null) {
-    inputValues = [];
-    for (let i = 0; i < inputArray.length; i++) {
-      inputValues.push(inputArray[i].value);
-      localStorage.setItem("timeInputs", JSON.stringify(inputValues));
-    }
+  inputValues = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    inputValues.push(inputArray[i].value);
+    localStorage.setItem("timeInputs", JSON.stringify(inputValues));
   }
-  renderInputs();
-  function renderInputs() {
-    for (let i = 0; i < inputArray.length; i++) {
-      inputArray[i].value = JSON.parse(localStorage.getItem("timeInputs"))[i];
-    }
+}
+renderInputs();
+function renderInputs() {
+  for (let i = 0; i < inputArray.length; i++) {
+    inputArray[i].value = JSON.parse(localStorage.getItem("timeInputs"))[i];
   }
+}
 
 //save user inputs on save buttons
 const buttons = $("button");
@@ -72,17 +84,17 @@ for (let button of buttons) {
 
 // save button appears when input is changed
 for (let i = 0; i < inputArray.length; i++) {
-    $(inputArray[i]).on("input", function (e) {
-      $(e.target)
-        .parent()
-        .parent()
-        .children()
-        .eq(1)
-        .children()
-        .eq(0)
-        .css("display", "block");
-    });
-  }
+  $(inputArray[i]).on("input", function (e) {
+    $(e.target)
+      .parent()
+      .parent()
+      .children()
+      .eq(1)
+      .children()
+      .eq(0)
+      .css("display", "block");
+  });
+}
 
 /* still to do -
 FIX time block colors appearing
@@ -93,8 +105,6 @@ Make readME
 
 
 */
-
-
 
 /*
 ***GIVEN I am using a daily planner to create a schedule
@@ -111,5 +121,3 @@ THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
 */
-
-
